dfdb04513c3e12008baea9d6ee8ded6374b86766
Kitch <rdub549@gmail.com>
2022-01-27 09:39:09 +0000

    Created bascii2bin for easier input>bin file

--- 2021/03_binary_diagnostic/c/script2.c
+++ 2021/03_binary_diagnostic/c/script2.c
@@ -5,16 +5,23 @@
 int main(void) {
 	int status = EXIT_SUCCESS;
 	FILE *inputfile;
-	if ((inputfile = fopen("../input","r")) == NULL ) {
+	if ((inputfile = fopen("../testinput.bin","rb")) == NULL ) {
 		fputs("Error reading input file\n",stderr);
 		return EXIT_FAILURE;
 	}
 	
 	//Variable Definitions
+	int byte;
 
-	while (fscanf(inputfile, "%s %d",dir,&amt) != EOF) {
-
+	while (1) {
+		byte = fgetc(inputfile);
+		if (byte == EOF) {
+			break;
+		}
+		printf("%x\n", byte);
 	}
+
+	printf("\n");
 
 	fclose(inputfile);
 	
