451283615368cc800dc8932b44e2275d54a86f77
Kitch <rdub549@gmail.com>
2022-01-28 03:54:29 +0000

    2021/03/2 Bin file read success

--- 2021/03_binary_diagnostic/c/script2.c
+++ 2021/03_binary_diagnostic/c/script2.c
@@ -2,28 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define DEBUG 1
+#define INPUTFILE "../testinput.bin"
+
+
 int main(void) {
-	int status = EXIT_SUCCESS;
+	
 	FILE *inputfile;
-	if ((inputfile = fopen("../testinput.bin","rb")) == NULL ) {
+	if ((inputfile = fopen(INPUTFILE,"rb")) == NULL ) {
 		fputs("Error reading input file\n",stderr);
-		return EXIT_FAILURE;
+		return 1;
 	}
 	
 	//Variable Definitions
-	int byte;
-
-	while (1) {
-		byte = fgetc(inputfile);
-		if (byte == EOF) {
-			break;
-		}
-		printf("%x\n", byte);
+	int byte[1000];
+	int i = 0;
+	int byte_length;
+	
+	if (DEBUG) printf("Reading File in While Loop\n");
+	while ((byte[i] = fgetc(inputfile)) != EOF) {
+		if (DEBUG) printf("i= %d\tbyte=%x\t\n", i, byte[i]);
+		i++;
 	}
-
-	printf("\n");
+	byte_length = i;
+	if (DEBUG) printf("\nOutside of While Loop\n");
+	if (DEBUG) printf("i= %d\tbyte=%x\tbyte_len=%d\n", i, byte[i], byte_length);
+	i = 0;
+	if (DEBUG) printf("\nInside of Index While Loop\n");
+	while (i < byte_length) {
+		if (DEBUG) printf("i= %d\tbyte=%x\tbyte_len=%d\n", i, byte[i], byte_length);
+		i++;
+	}
 
 	fclose(inputfile);
 	
-	return status;
+	return 0;
 }
