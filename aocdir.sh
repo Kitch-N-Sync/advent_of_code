14bfd64717d75a8a5189f8bb5d0684d508a95834
Kitch <rdub549@gmail.com>
2022-02-17 01:48:42 -0500

    compile errors

--- aocdir.sh
+++ aocdir.sh
@@ -9,22 +9,23 @@ i=1
 for year in $(ls $yearDIR); do
 	if [[ ${year:0:3} = "202" ]]; then
 		echo "$i: $year"
-		yeardirectories[$i]=("$yearDIR/$year/")
+		yeardirectories[$i]="$yearDIR/$year/"
 		((i+=1))
 	fi
 done
 read -p ">" yearchoice
-yearchoice=${yearchoice^^}
 isnum='^[0-9]+$'
 if [[ ${yearchoice^^} = "NEW" ]]; then
 	$yearchoice=0
 fi
 if [[ $yearchoice -eq 0 ]]; then
 	read -p "New Year [YYYY] >" addyear
-	read -p "Make new directory [$addyear]?\n[y/n] >" confirm;;
-else if [[ $yearchoice =~ $isnum ]]; then
+	echo "Make new directory [${addyear}]?" 
+	read -p "[y/n] >" confirm
+elif [[ $yearchoice =~ $isnum ]]; then
 	echo "Adding new directory to ${yeardirectories[1]}"
-	read -p "Enter directory name [00_name_of_puzzle]\n>"
+	echo "Enter directory name [00_name_of_puzzle]"
+	read -p "> " newdirname
 else
 	echo "Unknown option chosen"
 fi
